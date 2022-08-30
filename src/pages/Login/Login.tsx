f0d24fb14279cb18108500c48537148818ae758e
gerardotamo <gerardotamo@ravn.co>
2022-08-30 16:30:38 -0500

    build: add npm/apollo client

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom';
+import { useGeneralContext } from '../../shared/contexts/StoreProvider'
+import * as styled from './style';
 
 export const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    const context = useGeneralContext();
+    const uuid = context?.state.auth.admin.uuid;
+    if (uuid) {
+        return <Navigate to={"/"} />
+    }
+
+    return (
+        <styled.Container>
+            Login
+        </styled.Container>
+    )
 }
---
+++ src/pages/Login/style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { BaseColor } from '../../config/color';
+
+export const Container = styled('div')`
+    background: ${BaseColor.fieldColor};
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
