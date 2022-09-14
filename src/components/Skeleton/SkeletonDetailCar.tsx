8d3012d562348255ae06d1662212310b218f2e2f
gerardotamo <gerardotamo@ravn.co>
2022-09-14 10:55:34 -0500

    style: add skelon for car detail

--- src/components/Skeleton/SkeletonDetailCar.tsx
+++ src/components/Skeleton/SkeletonDetailCar.tsx
@@ -1,7 +1,128 @@
-import React from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import styled from "styled-components";
+import { BaseColor } from "../../config/color";
 
 const SkeletonDetailCar = () => {
-  return <div>SkeletonDetailCar</div>;
+  return (
+    <>
+      <SkeletonTheme
+        baseColor={BaseColor.blueDarkColor}
+        highlightColor={BaseColor.lightBluePrimaryColor}
+      >
+        <Header>
+          <Skeleton width={200} height={30} />
+        </Header>
+        <Column>
+          <Section>
+            <Skeleton width={"75vmin"} height={"60vmin"} />
+          </Section>
+          <DataContainer>
+            <InfoItem>
+              <Skeleton width={40} height={15} />
+              <Skeleton width={50} height={15} />
+            </InfoItem>
+
+            <InfoItem>
+              <Skeleton width={40} height={15} />
+              <Skeleton width={50} height={15} />
+            </InfoItem>
+
+            <InfoItem>
+              <Skeleton width={40} height={15} />
+              <Skeleton width={50} height={15} />
+            </InfoItem>
+
+            <InfoItem>
+              <Skeleton width={40} height={15} />
+              <Skeleton width={50} height={15} />
+            </InfoItem>
+
+            <InfoItem>
+              <Skeleton width={40} height={15} />
+              <Skeleton width={50} height={15} />
+            </InfoItem>
+
+            <InfoItem>
+              <Skeleton width={40} height={15} />
+              <Skeleton width={50} height={15} />
+            </InfoItem>
+
+            <InfoItem>
+              <Skeleton width={40} height={15} />
+              <Skeleton width={50} height={15} />
+            </InfoItem>
+
+            <InfoItem>
+              <Skeleton width={40} height={15} />
+              <Skeleton width={50} height={15} />
+            </InfoItem>
+
+            <InfoItem>
+              <Skeleton width={40} height={15} />
+              <Skeleton width={50} height={15} />
+            </InfoItem>
+
+            <InfoItem>
+              <Skeleton width={40} height={15} />
+              <Skeleton width={50} height={15} />
+            </InfoItem>
+
+            <InfoItem>
+              <Skeleton width={40} height={15} />
+              <Skeleton width={50} height={15} />
+            </InfoItem>
+          </DataContainer>
+        </Column>
+      </SkeletonTheme>
+    </>
+  );
 };
 
 export default SkeletonDetailCar;
+
+const Container = styled("div")`
+  color: white;
+  height: 100%;
+  padding-top: 80px;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Column = styled("div")`
+  display: flex;
+  margin-inline: 50px;
+  width: 70%;
+  justify-content: space-between;
+`;
+
+const Header = styled("div")`
+  display: flex;
+  width: 70%;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const InfoItem = styled("div")`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-block: 5px;
+`;
+
+const DataContainer = styled("div")`
+  box-shadow: 0 1px 6px rgba(0, 0, 0, 0.3);
+  margin-left: 15px;
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+`;
+
+const Section = styled("div")`
+  width: 100%;
+  display: flex;
+`;
--- src/pages/ViewCarDetail/ViewCarDetail.tsx
+++ src/pages/ViewCarDetail/ViewCarDetail.tsx
@@ -10,6 +10,7 @@ import { useGeneralContext } from "../../shared/contexts/StoreProvider";
 import { useEffect, useState } from "react";
 import ModalLoginVIew from "../../components/Modal/Modal";
 import FavoriteButton from "./FavoriteButton";
+import SkeletonDetailCar from "../../components/Skeleton/SkeletonDetailCar";
 
 export const ViewCarDetail = () => {
   const { carId } = useParams();
@@ -20,79 +21,85 @@ export const ViewCarDetail = () => {
   );
   const car = data?.cars[0];
 
+  //console.log(loading);
+
   return (
     <styled.Container>
-      {car ? (
-        <>
-          <styled.Header>
-            <styled.Title>{car.title}</styled.Title>
-            <FavoriteButton carData={data.cars} userCar={data.user_cars} />
-          </styled.Header>
-          <styled.Column>
-            <styled.Section>
-              <styled.Image src={Delorean} />
-            </styled.Section>
-            <styled.DataContainer>
-              <styled.InfoItem>
-                <styled.TextItem>VIN:</styled.TextItem>
-                <styled.TextItem>{car.vin}</styled.TextItem>
-              </styled.InfoItem>
-              <styled.Divider />
-              <styled.InfoItem>
-                <styled.TextItem>Odometer:</styled.TextItem>
-                <styled.TextItem>{car.odometer} mi</styled.TextItem>
-              </styled.InfoItem>
-              <styled.Divider />
-              <styled.InfoItem>
-                <styled.TextItem>Color:</styled.TextItem>
-                <styled.TextItem>{car.color.name}</styled.TextItem>
-              </styled.InfoItem>
-              <styled.Divider />
-              <styled.InfoItem>
-                <styled.TextItem>Brand</styled.TextItem>
-                <styled.TextItem>{car.brand.name}</styled.TextItem>
-              </styled.InfoItem>
-              <styled.Divider />
-              <styled.InfoItem>
-                <styled.TextItem>Model</styled.TextItem>
-                <styled.TextItem>{car.model.name}</styled.TextItem>
-              </styled.InfoItem>
-              <styled.Divider />
-              <styled.InfoItem>
-                <styled.TextItem>Condition</styled.TextItem>
-                <styled.TextItem>
-                  {car.condition === "A" ? "Salvage Damage" : "New"}
-                </styled.TextItem>
-              </styled.InfoItem>
-              <styled.Divider />
-              <styled.InfoItem>
-                <styled.TextItem>State</styled.TextItem>
-                <styled.TextItem>{car.state.name}</styled.TextItem>
-              </styled.InfoItem>
-              <styled.Divider />
-              <styled.InfoItem>
-                <styled.TextItem>City</styled.TextItem>
-                <styled.TextItem>{car.city.name}</styled.TextItem>
-              </styled.InfoItem>
-              <styled.Divider />
-              <styled.InfoItem>
-                <styled.TextItem>Price</styled.TextItem>
-                <styled.TextItem>{car.price}</styled.TextItem>
-              </styled.InfoItem>
-              <styled.Divider />
-              <styled.InfoItem>
-                <styled.TextItem>Year</styled.TextItem>
-                <styled.TextItem>{car.year}</styled.TextItem>
-              </styled.InfoItem>
-              <styled.Divider />
-              <styled.InfoItem>
-                <styled.TextItem>Saled Date</styled.TextItem>
-                <styled.TextItem>{car.sale_date}</styled.TextItem>
-              </styled.InfoItem>
-            </styled.DataContainer>
-          </styled.Column>
-        </>
-      ) : null}
+      {loading ? (
+        <SkeletonDetailCar />
+      ) : (
+        car && (
+          <>
+            <styled.Header>
+              <styled.Title>{car.title}</styled.Title>
+              <FavoriteButton carData={data.cars} userCar={data.user_cars} />
+            </styled.Header>
+            <styled.Column>
+              <styled.Section>
+                <styled.Image src={Delorean} />
+              </styled.Section>
+              <styled.DataContainer>
+                <styled.InfoItem>
+                  <styled.TextItem>VIN:</styled.TextItem>
+                  <styled.TextItem>{car.vin}</styled.TextItem>
+                </styled.InfoItem>
+                <styled.Divider />
+                <styled.InfoItem>
+                  <styled.TextItem>Odometer:</styled.TextItem>
+                  <styled.TextItem>{car.odometer} mi</styled.TextItem>
+                </styled.InfoItem>
+                <styled.Divider />
+                <styled.InfoItem>
+                  <styled.TextItem>Color:</styled.TextItem>
+                  <styled.TextItem>{car.color.name}</styled.TextItem>
+                </styled.InfoItem>
+                <styled.Divider />
+                <styled.InfoItem>
+                  <styled.TextItem>Brand</styled.TextItem>
+                  <styled.TextItem>{car.brand.name}</styled.TextItem>
+                </styled.InfoItem>
+                <styled.Divider />
+                <styled.InfoItem>
+                  <styled.TextItem>Model</styled.TextItem>
+                  <styled.TextItem>{car.model.name}</styled.TextItem>
+                </styled.InfoItem>
+                <styled.Divider />
+                <styled.InfoItem>
+                  <styled.TextItem>Condition</styled.TextItem>
+                  <styled.TextItem>
+                    {car.condition === "A" ? "Salvage Damage" : "New"}
+                  </styled.TextItem>
+                </styled.InfoItem>
+                <styled.Divider />
+                <styled.InfoItem>
+                  <styled.TextItem>State</styled.TextItem>
+                  <styled.TextItem>{car.state.name}</styled.TextItem>
+                </styled.InfoItem>
+                <styled.Divider />
+                <styled.InfoItem>
+                  <styled.TextItem>City</styled.TextItem>
+                  <styled.TextItem>{car.city.name}</styled.TextItem>
+                </styled.InfoItem>
+                <styled.Divider />
+                <styled.InfoItem>
+                  <styled.TextItem>Price</styled.TextItem>
+                  <styled.TextItem>{car.price}</styled.TextItem>
+                </styled.InfoItem>
+                <styled.Divider />
+                <styled.InfoItem>
+                  <styled.TextItem>Year</styled.TextItem>
+                  <styled.TextItem>{car.year}</styled.TextItem>
+                </styled.InfoItem>
+                <styled.Divider />
+                <styled.InfoItem>
+                  <styled.TextItem>Saled Date</styled.TextItem>
+                  <styled.TextItem>{car.sale_date}</styled.TextItem>
+                </styled.InfoItem>
+              </styled.DataContainer>
+            </styled.Column>
+          </>
+        )
+      )}
     </styled.Container>
   );
 };
