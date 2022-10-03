71554c3daab2ded8a996aa337c04fd6f585c40ed
gerardotamo <gerardotamo@ravn.co>
2022-10-03 09:42:25 -0500

    feat: add static data for test

---
+++ src/shared/utils/data/StateProvider.ts
@@ -0,0 +1,16 @@
+const stateProvider = () => {
+  return {
+    auth: {
+      admin: {
+        email: 'gerardotamo@ravn.co',
+        first_name: 'Gerardo',
+        id: 1,
+        last_name: 'Tamo',
+        uuid: 'f234050c-be3c-4f93-afe0-c763bd6fb492',
+        __typename: 'users',
+      },
+    },
+  };
+};
+
+export default stateProvider;
